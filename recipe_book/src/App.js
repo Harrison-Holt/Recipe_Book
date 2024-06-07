cb3ad125e922dea528390cd1c27d160daf2ec986
Harrison-Holt <hholt2901@gmail.com>
2024-06-07 13:39:07 -0400

    routing

--- recipe_book/src/App.js
+++ recipe_book/src/App.js
@@ -2,19 +2,26 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
-
+const Header = lazy(() => import('./UI/header'));
+const Footer = lazy(() => import('./UI/hooter'));
+const HomePage = lazy(() => import('./Pages/HomePage'));
+const Register = lazy(() => import('./Pages/register'));
+const Login = lazy(() => import('./Pages/login')); 
 
 function App() {
   return (
-    <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/user" element={<UserPage />} />
-        <Route path="/home" element={<HomePage />} />
-      </Routes>
-    </BrowserRouter>
+    <Router>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Suspense>
+  </Router>
   );
 }
 
---
+++ recipe_book/src/Pages/HomePage.js
@@ -0,0 +1,11 @@
+import react from 'react'; 
+
+function HomePage() {
+    return (
+        <body>
+            <p>homepage</p>
+        </body>
+    ); 
+}
+
+export default HomePage; 
